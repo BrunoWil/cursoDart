2aebf6620754bc8c64ff685650e77dc7c3d91775
Bruno Wilson <bruno.wilson.m@gmail.com>
2023-08-29 12:12:49 -0300

    atividade3

---
+++ lib/src/blocs/movie_detail_bloc.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:http/http.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../models/trailer_model.dart';
+import '../resources/repository.dart';
+
+class MovieDetailBloc {
+  final _repository = Repository();
+  final _movieId = PublishSubject<int>();
+  final _trailers = BehaviorSubject<Future<TrailerModel>>();
+
+  Function(int) get fetchTrailersById => _movieId.sink.add;
+  Stream<Future<TrailerModel>> get movieTrailers => _trailers.stream;
+
+  MovieDetailBloc() {
+    _movieId.stream.transform(_itemTransformer()).pipe(_trailers);
+  }
+  
+  get id => null;
+
+  dispose() async {
+    _movieId.close();
+    await _trailers.drain();
+    _trailers.close();
+  }
+
+  _itemTransformer() {
+    return ScanStreamTransformer(
+      (Future<TrailerModel> trailer, int id, int index) {
+        print(index);
+        trailer = _repository.fetchTrailers(id);
+        return trailer;
+      }, id as Future<TrailerModel>, 
+    );
+  }
+  
+}
